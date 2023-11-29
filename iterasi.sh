369f38336921f0221fd2f0ceb28d740e5e623c41
Numbu Ranggata <numburanggata@gmail.com>
2023-11-30 06:59:34 +0700

    Update iterasi.sh

--- iterasi.sh
+++ iterasi.sh
@@ -4,8 +4,9 @@
 num_iterations="100"
 
 # Base command with fixed parameters
-base_command="./waf --run 'iot_PUF_eval --M1=68 --M2=44 --M3=36 --M4=52 " #64-bit REI
-base_command="./waf --run 'iot_PUF_eval --M1=78 --M2=48 --M3=38 --M4=58 " #80-bit REI
+base_command="./waf --run 'iot_PUF_eval --M1=188 --M2=92 --M3=60 --M4=124 " #64-bit REI
+#base_command="./waf --run 'iot_PUF_eval --M1=68 --M2=44 --M3=36 --M4=52 " #64-bit REI
+#base_command="./waf --run 'iot_PUF_eval --M1=78 --M2=48 --M3=38 --M4=58 " #80-bit REI
 #base_command="./waf --run 'iot_PUF_eval --M1=108 --M2=60 --M3=44 --M4=76 " #128-bit FIN
 #base_command="./waf --run 'iot_PUF_eval --M1=113 --M2=62 --M3=45 --M4=79 " #136-bit FIN
 #base_command="./waf --run 'iot_PUF_eval --M1=128 --M2=68 --M3=48 --M4=88 " #160-bit FIN
